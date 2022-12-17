f47603708ea42b53ea6bdf8919843e10e9e77a74
rauch <rauchrodrigues@gmail.com>
2022-12-17 14:46:44 +0530

    yo1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -21,6 +21,33 @@ public class Car extends Vehicle {
         changeGear(1);
     }
 
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
 
     public void changeGear(int newGear){
         currentGear=newGear;
